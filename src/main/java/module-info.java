0001cec2bff48993225fa7f138b1c4af4e61c7cd
Roody <roodyalexis36@gmail.com>
2025-04-08 16:36:31 +0200

    added screen pages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,16 @@
 module com.thaus.chatbox {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires transitive javafx.graphics;
 
 	requires org.controlsfx.controls;
 	requires com.dlsc.formsfx;
 	requires org.kordamp.bootstrapfx.core;
 
 	opens com.thaus.chatbox to javafx.fxml;
+
 	exports com.thaus.chatbox;
 	exports com.thaus.chatbox.controllers;
+
 	opens com.thaus.chatbox.controllers to javafx.fxml;
 }
